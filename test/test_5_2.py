99017a40f927fb177e05762038324a0816ed4d09
Ejercicios_DAW_Python <aarrieta@iesrafaelalberti.es>
2023-10-17 09:27:13 +0200

    Arreglo Test 5

--- test/test_5_2.py
+++ test/test_5_2.py
@@ -2,4 +2,4 @@ from src.Ej_5_2 import inversion
 
 
 def test_inversion():
-    assert inversion(900, 15, 3) == ""
+    assert inversion(900, 15, 3) == ("Año 1 -- 1035.0\nAño 2 -- 1190.25\nAño 3 -- 1368.79\n")
